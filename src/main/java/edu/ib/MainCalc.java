99a59bf4f09dae5665da42358edd651198041b62
Jan Polański <250034@student.pwr.edu.pl>
2020-12-14 22:14:43 +0100

    Initial commit

---
+++ src/main/java/edu/ib/MainCalc.java
@@ -0,0 +1,27 @@
+package edu.ib;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainCalc extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/calculator.fxml"));
+        Scene scene = new Scene(root,600,500);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
